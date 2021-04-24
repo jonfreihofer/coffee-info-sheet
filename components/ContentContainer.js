11da6fc7150185dff8f44ea04575f2414bc2b71c
Jonathan Freihofer <patientfrei@gmail.com>
2021-04-24 10:16:43 -0400

    convert

--- components/ContentContainer.js
+++ components/ContentContainer.js
@@ -1,31 +1,21 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 import Aside from "./Aside";
 import MainContent from "./MainContent";
 
-class ContentContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selected: "thePlant",
-    };
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-
-  handleSelect = option => {
-    this.setState({selected: option});
+function ContentContainer(props) {
+  const [selected, setSelected] = useState("theCost");
+  const handleSelect = option => {
+    setSelected({ option });
   };
-
-  render() {
-    return (
-      <div className="container-main">
-        <Aside handleSelect={this.handleSelect} />
-        <MainContent
-          selected={this.state.selected}
-          handleSelect={this.handleSelect}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="container-main">
+      <Aside handleSelect={handleSelect} />
+      <MainContent
+        selected={selected}
+        handleSelect={handleSelect}
+      />
+    </div>
+  );
 }
 
 export default ContentContainer;
