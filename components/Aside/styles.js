86d6faed8eb4c996df2f304a802cbfa5fe00c5ac
Jonathan Freihofer <patientfrei@gmail.com>
2021-06-01 19:31:34 -0400

    changed that from looking horrible

--- components/Aside/styles.js
+++ components/Aside/styles.js
@@ -4,7 +4,6 @@ export const StyledAside = styled.aside`
   li {
     text-align: center;
     border: solid 1px orangered;
-    border-radius: 47%;
     margin: 2rem;
     padding: 1rem;
     width: 125px;
