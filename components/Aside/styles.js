31b5b85c218747f60ec9db3a42113dbd459bc147
Jonathan Freihofer <patientfrei@gmail.com>
2021-06-02 08:45:32 -0400

    added margin-images

--- components/Aside/styles.js
+++ components/Aside/styles.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const StyledAside = styled.aside`
   li {
     background: #1c1c1c;
-    letter-spacing: 3px;
+    letter-spacing: 2px;
+    text-transform: uppercase;
     font-weight: bold;
     color: white;
     text-align: center;
---
+++ components/PriceInfo/index.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+
+export default function PriceInfo({ children }) {
+  return <>{children}</>;
+}
