198afc19a7611c88a310c54dc1c50a716596d122
Jonathan Freihofer <patientfrei@gmail.com>
2021-06-01 20:14:18 -0400

    hover effect

--- components/Aside/styles.js
+++ components/Aside/styles.js
@@ -2,10 +2,16 @@ import styled from "styled-components";
 
 export const StyledAside = styled.aside`
   li {
+    background: #1c1c1c;
+    letter-spacing: 3px;
+    font-weight: bold;
+    color: white;
     text-align: center;
-    border: solid 1px orangered;
     margin: 2rem;
     padding: 1rem;
     width: 125px;
+    &:hover {
+      background: grey;
+    }
   }
 `;
