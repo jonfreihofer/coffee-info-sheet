309df5849867a47843e260c2d52841f5e1e2f2af
Jonathan Freihofer <patientfrei@gmail.com>
2021-07-16 10:13:33 -0400

    implement image render function

---
+++ components/ImageContainer/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyledImageContainer } from "./styles";
+
+export default function ImageContainer({ children }) {
+  return <StyledImageContainer>{children}</StyledImageContainer>;
+}
--- components/MainContent.js
+++ components/MainContent.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PriceInfo from "./PriceInfo";
+import ImageContainer from "./ImageContainer";
 // eslint-disable-next-line react/prop-types
 export default function MainContent({ selected }) {
   const [data, setData] = useState({});
@@ -12,9 +13,7 @@ export default function MainContent({ selected }) {
   }, []);
   // helper function just for now, to save me from being annoyed at all the broken images
   const renderImg = (source) => {
-    return (
-      source && <img src={source} alt="a pic of something related to coffee" />
-    );
+    return source && <img src={source} alt="something related to coffee" />;
   };
 
   const currentPageData = data[selected];
@@ -24,8 +23,10 @@ export default function MainContent({ selected }) {
       <h1>{currentPageData.title}</h1>
       <p>{currentPageData.content}</p>
       {currentPageData.title === "Cost" && <PriceInfo />}
-      {renderImg(currentPageData.img)}
-      {renderImg(currentPageData.img2)}
+      <ImageContainer>
+        {renderImg(currentPageData.img)}
+        {renderImg(currentPageData.img2)}
+      </ImageContainer>
     </main>
   );
 }
